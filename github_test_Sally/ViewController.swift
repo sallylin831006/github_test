52aa876ff83347534c225d77c5b2bcbc1610b86e
sallylin <missikun.tw@gmail.com>
2022-06-16 00:15:02 +0800

    Add git ignore and new xcode project

---
+++ github_test_Sally/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github_test_Sally
+//
+//  Created by 林翊婷 on 2022/6/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
