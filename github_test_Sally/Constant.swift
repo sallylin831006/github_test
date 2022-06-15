43620cf3534e67850cd208ef5646bf86398e3bf0
sallylin <missikun.tw@gmail.com>
2022-06-16 00:47:57 +0800

    Add gitHub Domain  url

---
+++ github_test_Sally/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  github_test_Sally
+//
+//  Created by 林翊婷 on 2022/6/16.
+//
+
+import Foundation
+
+struct Constant {
+    
+    static let gitHubDomain = "gitHubURL"
+   
+}
